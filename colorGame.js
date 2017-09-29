5e176079b1f00ee218a0ee3b26f70d24db552316
savini419 <stevensavini@gmail.com>
2017-09-29 10:31:32 -0400

    hard coded squares to ensure logic was correct

--- colorGame.js
+++ colorGame.js
@@ -7,9 +7,26 @@ let colors = [
   "rgb(255, 0, 255)",
 ];
 
-
+let pickedColor = colors[3];
+let colorDisplay = document.getElementById("colorDisplay");
 let squares = document.querySelectorAll(".square");
 
+colorDisplay.textContent = pickedColor;
+
 for(let i = 0; i < squares.length; i++) {
+  //add initial colors to squares
   squares[i].style.backgroundColor = colors[i];
+
+  //add click listeners to squares
+  squares[i].addEventListener("click", function() {
+    let clickedColor = this.style.backgroundColor;
+    //compare color to clicked color
+    if (clickedColor === pickedColor) {
+      alert("Correct!");
+    }
+    else {
+      alert("Wrong!");
+    }
+
+  })
 }
