0db139cfba253751af497ceda3cdb7dbf5a60495
savini419 <stevensavini@gmail.com>
2017-09-29 11:32:47 -0400

    randomized the squares

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,10 @@
-let colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)",
-];
+let colors = generateRandomColors(6);
 
-let pickedColor = colors[3];
+let squares = document.getElementsByClassName("square");
+let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
-let squares = document.querySelectorAll(".square");
+let messageDisplay = document.getElementById("message")
+let h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -22,11 +17,47 @@ for(let i = 0; i < squares.length; i++) {
     let clickedColor = this.style.backgroundColor;
     //compare color to clicked color
     if (clickedColor === pickedColor) {
-      alert("Correct!");
+      messageDisplay.textContent = "Correct";
+      changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
+    } else {
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again"
     }
-    else {
-      alert("Wrong!");
-    }
-
   })
+}
+
+function changeColors(color) {
+  //loop through all squares
+  for(let i = 0; i < squares.length; i++){
+    //change each color to match given color
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor(){
+  let random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateRandomColors(num){
+  //make array
+  let arr = [];
+  //put random colors into array
+    for (let i = 0; i < num; i++) {
+      arr.push(randomColor());
+    }
+  //return the array
+  return arr;
+}
+
+function randomColor() {
+    //pick a red from 0-255
+    let r = Math.floor(Math.random() * 255)
+    //pick a green from 0-255
+    let g = Math.floor(Math.random() * 255)
+    //pick a blue from 0-255
+    let b = Math.floor(Math.random() * 255)
+    //return the color as a string
+    return "rgb(" + r + ", " + g + ", " + b + ")"
 }
